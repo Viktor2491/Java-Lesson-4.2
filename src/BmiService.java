d0e9e388250e73701320e045d5f7328b4f745ff8
viktor91 <v.a.taradaiko@gmail.com>
2022-06-16 12:00:52 +0400

    Final version

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
     public double calculate(double m, double h) {
-    double result = m / ( h * h );
-    return result;
+        double x = h / 100;
+        double result = m / (x * x);
+        return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 70;
-        double height = 1.81;
+        double weight = 70; // ввести значение в килограммах
+        double height = 181; // ввести значение в сантиметрах
         double bmi = service.calculate(weight, height);
         System.out.println("Ваш индекс массы тела:");
         System.out.println(bmi);
